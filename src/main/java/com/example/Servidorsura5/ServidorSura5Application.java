b8f9933205c8a531274ae096a4a3e34e025a0c2d
Zorany <zorany_26.pc@hotmail.com>
2024-11-25 11:50:47 -0500

    Se configuró el CORS (no está completo).

--- src/main/java/com/example/Servidorsura5/ServidorSura5Application.java
+++ src/main/java/com/example/Servidorsura5/ServidorSura5Application.java
@@ -2,6 +2,9 @@ package com.example.Servidorsura5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ServidorSura5Application {
@@ -9,5 +12,16 @@ public class ServidorSura5Application {
 	public static void main(String[] args) {
 		SpringApplication.run(ServidorSura5Application.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer  corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")  // Permite todas las rutas
+						.allowedOrigins("*")  // Permite cualquier origen
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Métodos permitidos
+						.allowedHeaders("*");  // Permite todos los encabezados
+			}
+		};
+	}
 }
